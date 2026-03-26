1714f83be1e3b09440b01ddd6e24de9fd098d964
iamsnaaz <sadiyanaaz4255@gmail.com>
2026-03-26 11:44:52 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "poc-eks"
+  cluster_version = "1.29"
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+    }
+  }
+}
