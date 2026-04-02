4b4545f29dbac6e8c8e80a257a76e662c28b859e
iamsnaaz <sadiyanaaz4255@gmail.com>
2026-04-02 09:46:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,48 +1,19 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
-  default     = "poc-eks"
 }
 
-variable "cluster_version" {
-  description = "EKS version"
-  type        = string
-  default     = "1.29"
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
+variable "instance_type" {
+  description = "EC2 instance type"
   type        = string
 }
 
-variable "subnet_ids" {
-  description = "Subnet IDs"
-  type        = list(string)
-}
-
-variable "node_instance_type" {
-  description = "Worker node instance type"
-  type        = string
-  default     = "t3.medium"
-}
-
-variable "desired_size" {
-  type    = number
-  default = 2
-}
-
-variable "max_size" {
-  type    = number
-  default = 3
-}
-
-variable "min_size" {
-  type    = number
-  default = 1
+variable "desired_capacity" {
+  description = "Number of worker nodes"
+  type        = number
 }
