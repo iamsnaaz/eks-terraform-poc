4c2f41ed313952d8a4316436a01aa178253b2c5b
iamsnaaz <sadiyanaaz4255@gmail.com>
2026-04-02 09:49:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,55 @@
+provider "aws" {
+  region = var.region
+}
+
+# ------------------------
+# VPC
+# ------------------------
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.region}a", "${var.region}b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+# ------------------------
+# EKS Cluster
+# ------------------------
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "20.0.0"
 
-  cluster_name    = "poc-eks"
+  cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
-  vpc_id     = var.vpc_id
-  subnet_ids = var.subnet_ids
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
+      desired_size = var.desired_capacity
       max_size     = 3
       min_size     = 1
+
+      instance_types = [var.instance_type]
     }
+  }
+
+  tags = {
+    Environment = "poc"
   }
 }
