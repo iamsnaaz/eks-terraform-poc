53de769d23522251dd7a2a841dbf9576dd997749
iamsnaaz <sadiyanaaz4255@gmail.com>
2026-04-02 09:45:35 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,11 +1,18 @@
 terraform {
-  required_version = ">= 1.3"
+  required_version = ">= 1.3.0"
 
   cloud {
-    organization = "your-org"
+    organization = "poc_eks"
 
     workspaces {
       name = "eks-poc"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
